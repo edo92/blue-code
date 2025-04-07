febcc4175fee1c8f61e01cf1086d3ac93ebf575e
edo92 <edoter92@gmail.com>
2025-04-07 12:14:38 -0700

    update setup script

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gl-inet-v2",
+    name="blue-code",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        "pyserial",
+    ],
+    entry_points={
+        'console_scripts': [
+            'blue-code=src.cli:main',
+        ],
+    },
+    python_requires='>=3.6',
+    description="BlueCode Security Tools to enhance anonymity and reduce forensic traceability",
+    author="GL-iNet",
 )
