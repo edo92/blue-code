c66a170ffbf861e5912294985704e1ee916d2850
edo92 <edoter92@gmail.com>
2025-04-07 11:55:22 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-## Install Python
-# opkg update
-# opkg install python3
-# opkg install python3-pip
+# Install Python
+opkg update
+opkg install python3
+opkg install python3-pip
 
-## Create switch button
-# mkdir /etc/hotplug.d/button
-# cp ./src/etc/50-toggle_display /etc/hotplug.d/button/
-# chmod +x /etc/hotplug.d/button/50-toggle_display
-# /etc/init.d/e750_mcu restart
+# Install packages
+pip3 install -e .
 
-## Install packages
-# pip3 install -e .
+# Create switch button
+mkdir /etc/hotplug.d/button
+cp ./src/etc/50-toggle_display /etc/hotplug.d/button/
+chmod +x /etc/hotplug.d/button/50-toggle_display
+/etc/init.d/e750_mcu restart
 
-## Install security script
+# Install security script
 chmod +x src/etc/boot-security.sh
 sh src/etc/boot-security.sh
